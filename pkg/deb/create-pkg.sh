a99d0c42a0d51fdc604a120381483db24f771265
Michał Łyszczek <michal.lyszczek@bofc.pl>
2018-11-14 22:06:49 +0100

    pkg: add basic files to create packages
    no_buildbot

---
+++ pkg/deb/create-pkg.sh
@@ -0,0 +1,128 @@
+#!/bin/sh
+
+scp_server="pkgs@kurwik"
+project="embedlog"
+
+if [ ${#} -ne 3 ]
+then
+    echo "usage: ${0} <version> <arch> <host_os>"
+    echo ""
+    echo "where:"
+    echo "    <version>         git tag version to build (without prefix v)"
+    echo "    <arch>            target architecture"
+    echo "    <host_os>         target os (debian9, debian8 etc)"
+    echo ""
+    echo "example"
+    echo "      ${0} 1.0.0 i386 debian9"
+    exit 1
+fi
+
+version="${1}"
+arch="${2}"
+host_os="${3}"
+
+###
+# preparing
+#
+
+rm -rf "/tmp/${project}-${version}"
+mkdir "/tmp/${project}-${version}"
+
+cd "/tmp/${project}-${version}"
+git clone "https://git.kurwinet.pl/${project}"
+cd "${project}"
+
+git checkout "${version}" || exit 1
+
+if [ ! -d "pkg/deb" ]
+then
+    echo "pkg/deb does not exist, cannot create debian pkg"
+    exit 1
+fi
+
+version="$(grep "AC_INIT(" "configure.ac" | cut -f3 -d\[ | cut -f1 -d\])"
+abi_version="$(echo ${version} | cut -f1 -d.)"
+
+echo "version ${version}"
+echo "abi version ${abi_version}"
+
+###
+# building package
+#
+
+codename="$(lsb_release -c | awk '{print $2}')"
+
+cp -r "pkg/deb/" "debian"
+sed -i "s/@{DATE}/$(date -R)/" "debian/changelog.template"
+sed -i "s/@{VERSION}/${version}/" "debian/changelog.template"
+sed -i "s/@{CODENAME}/${codename}/" "debian/changelog.template"
+sed -i "s/@{ABI_VERSION}/${abi_version}/" "debian/control.template"
+
+mv "debian/changelog.template" "debian/changelog"
+mv "debian/control.template" "debian/control"
+
+debuild -us -uc || exit 1
+
+###
+# verifying
+#
+
+cd ..
+
+# debuild doesn't fail when lintial finds an error, so we need
+# to check it manually, it doesn't take much time, so whatever
+
+for d in *.deb
+do
+    echo "Running lintian on ${d}"
+    lintian ${d} || exit 1
+done
+
+dpkg -i "${project}${abi_version}_${version}_${arch}.deb" || exit 1
+dpkg -i "${project}-dev_${version}_${arch}.deb" || exit 1
+
+failed=0
+gcc ${project}/pkg/test.c -o testprog -lembedlog || failed=1
+
+if ldd ./testprog | grep "\/usr\/bofc"
+then
+    # sanity check to make sure test program uses system libraries
+    # and not locally installed ones (which are used as build
+    # dependencies for other programs
+
+    echo "test prog uses libs from manually installed /usr/bofc \
+        instead of system path!"
+    failed=1
+fi
+
+./testprog || failed=1
+
+dpkg -r "${project}${abi_version}" "${project}-dev" || exit 1
+
+if [ ${failed} -eq 1 ]
+then
+    exit 1
+fi
+
+if [ -n "${scp_server}" ]
+then
+    dbgsym_pkg="${project}${abi_version}-dbgsym_${version}_${arch}.deb"
+
+    if [ ! -f "${dbgsym_pkg}" ]
+    then
+        # on some systems packages with debug symbols are created with
+        # ddeb extension and not deb
+        dbgsym_pkg="${project}${abi_version}-dbgsym_${version}_${arch}.ddeb"
+    fi
+
+    echo "copying data to ${scp_server}:${project}/${host_os}/${arch}"
+    scp "${project}-dev_${version}_${arch}.deb" \
+        "${dbgsym_pkg}" \
+        "${project}${abi_version}_${version}_${arch}.deb" \
+        "${project}_${version}.dsc" \
+        "${project}_${version}.tar.xz" \
+        "${project}_${version}_${arch}.build" \
+        "${project}_${version}_${arch}.buildinfo" \
+        "${project}_${version}_${arch}.changes" \
+        "${scp_server}:${project}/${host_os}/${arch}" || exit 1
+fi
---
+++ pkg/rpm/create-pkg.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+
+project="embedlog"
+scp_server="pkgs@kurwik"
+
+if [ ${#} -ne 3 ]
+then
+    echo "usage: ${0} <version> <arch> <host_os>"
+    echo ""
+    echo "where"
+    echo "    <version>        git tag version"
+    echo "    <arch>           target architecture"
+    echo "    <host_os>        target os (rhel-7, centos-7 etc)"
+    exit 1
+fi
+
+git_version="${1}"
+arch="${2}"
+host_os="${3}"
+
+cd "${HOME}/rpmbuild"
+pkg_version="$(curl "https://git.kurwinet.pl/${project}/plain/configure.ac?h=${git_version}" | \
+    grep "AC_INIT(" | cut -f3 -d\[ | cut -f1 -d\])"
+wget "https://git.kurwinet.pl/${project}/snapshot/${project}-${git_version}.tar.gz" \
+    -O "SOURCES/${project}-${pkg_version}.tar.gz"
+wget "https://git.kurwinet.pl/${project}/plain/pkg/rpm/${project}.spec.template?h=${git_version}" \
+    -O "SPECS/${project}-${pkg_version}.spec"
+lt_version="$(curl "https://git.kurwinet.pl/${project}/plain/src/Makefile.am?h=${git_version}" | \
+    grep "${project}_la_LDFLAGS = -version-info" | cut -f4 -d\ )"
+
+current="$(echo ${lt_version} | cut -f1 -d:)"
+revision="$(echo ${lt_version} | cut -f2 -d:)"
+age="$(echo ${lt_version} | cut -f3 -d:)"
+
+lib_version="$(( current - age )).${age}.${revision}"
+abi_version="$(( current - age ))"
+rel_version="$(cat SPECS/${project}-${pkg_version}.spec | \
+    grep "Release:" | awk '{print $2}')"
+
+sed -i "s/@{VERSION}/${pkg_version}/" SPECS/${project}-${pkg_version}.spec
+sed -i "s/@{GIT_VERSION}/${git_version}/" SPECS/${project}-${pkg_version}.spec
+sed -i "s/@{LIB_VERSION}/${lib_version}/" SPECS/${project}-${pkg_version}.spec
+sed -i "s/@{ABI_VERSION}/${abi_version}/" SPECS/${project}-${pkg_version}.spec
+
+
+if cat /etc/os-release | grep "openSUSE Leap"
+then
+    # opensuse doesn't generate debug symbols by defaul, check spec file
+    # for comment
+    sed -i 's/# __DEBUG_PACKAGE__/%debug_package/' \
+        SPECS/${project}-${pkg_version}.spec
+fi
+
+rpmbuild -ba SPECS/${project}-${pkg_version}.spec || exit 1
+
+###
+# verify
+#
+
+if type zypper >/dev/null
+then
+    # looks like we are dealing with opensuse
+
+    zypper install -y --allow-unsigned-rpm \
+        "RPMS/${arch}/${project}-${pkg_version}-${rel_version}.${arch}.rpm" \
+        "RPMS/${arch}/${project}-devel-${pkg_version}-${rel_version}.${arch}.rpm"
+else
+    # else, assume rhel or centos or fedora or whatever that uses yum
+
+    yum -y install \
+        "RPMS/${arch}/${project}-${pkg_version}-${rel_version}.${arch}.rpm" \
+        "RPMS/${arch}/${project}-devel-${pkg_version}-${rel_version}.${arch}.rpm"
+fi
+
+failure=0
+gcc "BUILD/${project}-${git_version}/pkg/test.c" -lembedlog -o /tmp/${project}-test || failure=1
+
+if ldd /tmp/${project}-test | grep "\/usr\/bofc"
+then
+    # sanity check to make sure test program uses system libraries
+    # and not locally installed ones (which are used as build
+    # dependencies for other programs
+
+    echo "test prog uses libs from manually installed /usr/bofc \
+        instead of system path!"
+    failed=1
+fi
+
+/tmp/${project}-test || failure=1
+
+if type zypper >/dev/null
+then
+    zypper remove -y "${project}" "${project}-devel"
+else
+    yum -y remove "${project}" "${project}-devel"
+fi
+
+if [ ${failure} -eq 1 ]
+then
+    exit 1
+fi
+
+if [ -n "${scp_server}" ]
+then
+    echo "copying data to ${scp_server}:${project}/${host_os}/${arch}"
+    scp "RPMS/${arch}/${project}-${pkg_version}-${rel_version}.${arch}.rpm" \
+        "RPMS/${arch}/${project}-devel-${pkg_version}-${rel_version}.${arch}.rpm" \
+        "RPMS/${arch}/${project}-debuginfo-${pkg_version}-${rel_version}.${arch}.rpm" \
+        "${scp_server}:${project}/${host_os}/${arch}" || exit 1
+fi
