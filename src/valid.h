3efe227e96843c72712e9e97d2ee1f2f5c0e1b81
Michał Łyszczek <michal.lyszczek@bofc.pl>
2019-05-17 19:48:37 +0200

    src/valid.h: add some more VALID macros
    Signed-off-by: Michał Łyszczek <michal.lyszczek@bofc.pl>

--- src/valid.h
+++ src/valid.h
@@ -8,24 +8,62 @@
 #define EL_VALID_H 1
 
 
-/* ==== include files ======================================================= */
-
-
 #include <errno.h>
 
 
-/* ==== public macros ======================================================= */
-
-
 /* ==========================================================================
-    If expression 'x' evaluates to false,  macro will set errno value to 'e'
+    If expression 'x' evaluates to false, macro will set errno value to 'e'
     and will force function to return with code '-1'
    ========================================================================== */
 
 
-#define VALID(e ,x) if (!(x)) { errno = (e); return -1; }
+#define VALID(e, x) if (!(x)) { errno = (e); return -1; }
 
 
-#define VALIDGO(e, x, l) if (!(x)) { errno = (e); goto l; }
+/* ==========================================================================
+    Same as VALID but when expression fails, message is printed to default
+    embedlog facility
+   ========================================================================== */
+
+
+#define VALIDP(e, x) if (!(x)) { el_print(ELW, "VALID FAILED "#x); \
+    errno = (e); return -1; }
+
+
+/* ==========================================================================
+    Same as VALIDP but message is printed to default option, defined by
+    EL_OPTIONS_OBJECT
+   ========================================================================== */
+
+
+#define VALIDOP(e, x) if (!(x)) { el_oprint(OELW, "VALID FAILED "#x); \
+    errno = (e); return -1; }
+
+
+/* ==========================================================================
+    If expression 'x' evaluates to false, macro will set errno value to 'e'
+    and will force function to return value 'v'
+   ========================================================================== */
+
+
+#define VALIDR(e, v, x) if (!(x)) { errno = (e); return (v); }
+
+
+/* ==========================================================================
+    If expression 'x' evaluates to false, macro will set errno value to 'e'
+    and will jump to lable 'l'
+   ========================================================================== */
+
+
+#define VALIDG(e, l, x) if (!(x)) { errno = (e); goto l; }
+
+
+/* ==========================================================================
+    If expression 'x' evaluates to false, macro will call code 'c', set
+    errno to 'e', and return -1
+   ========================================================================== */
+
+
+#define VALIDC(e, x, c) if (!(x)) { c; errno = (e); return -1; }
 
 #endif
