dd3c886c9e7403e2a41296a79cee0157893de2ed
Michał Łyszczek <michal.lyszczek@bofc.pl>
2020-05-29 19:04:17 +0200

    pvs-analize.sh: add script to run pvs-analyzer on the code
    Signed-off-by: Michał Łyszczek <michal.lyszczek@bofc.pl>

---
+++ pvs-analyze.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+make clean
+pvs-studio-analyzer trace -- make
+pvs-studio-analyzer analyze -o pvs-studio.log --disableLicenseExpirationCheck
+plog-converter -a "GA:1,2;64:1;OP:1,2,3;CS:1;MISRA:1,2" -t tasklist \
+    -o report.tasks pvs-studio.log
