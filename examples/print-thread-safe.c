b5682c57099c285ccc7cabdf59b00d431981ea6a
Michał Łyszczek <michal.lyszczek@bofc.pl>
2021-01-25 00:39:58 +0100

    examples: add missing el_cleanup() to some examples
    Signed-off-by: Michał Łyszczek <michal.lyszczek@bofc.pl>

--- examples/print-thread-safe.c
+++ examples/print-thread-safe.c
@@ -16,13 +16,14 @@ int main(void)
 	if (el_option(EL_THREAD_SAFE, 1) != 0)
 	{
 		el_perror(ELF, "Failed to enable thread safety");
-		goto error;
+		el_cleanup();
+		return 1;
 	}
 
 	el_print(ELI, "Yeaaa.... there is no really good way to show");
 	el_print(ELI, "how pthread works in action, so this just checks");
 	el_print(ELI, "whether locking works at all");
 
-error:
 	el_cleanup();
+	return 0;
 }
